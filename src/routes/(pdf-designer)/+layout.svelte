9696a2b3ddaafd28092c402db6729beb07486499
Monika Buchtíková <buchtikovam@email.cz>
2024-09-03 15:31:22 +0200

    pdf generation

--- src/routes/(pdf-designer)/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import './../../app.pcss';
-</script>
-
-<div class="h-dvh w-dvh bg-albi-50">
-
-	<slot />
-</div>
--- src/routes/(pdf-designer)/pdf/test/[testId]/+page.svelte
+++ src/routes/(pdf-designer)/pdf/test/[testId]/+page.svelte
@@ -1,74 +1,214 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
-
-	let element: HTMLElement | null;
-
-	let data = {
-		invoiceNumber: 147258369,
-		orderNumber: 3692581475,
-	}
-
-	onMount(() => {	
-		element = document.getElementById('element-to-print');
-	})
-
-	function printing() {
-        const content = document.getElementById('content');
-		const filename = 'table_data.pdf';
-
-		var opt = {
-			margin: 0,
-			filename: `faktura-${data.invoiceNumber}.pdf`,
-			image: { type: 'jpeg', quality: 1 },
-			html2canvas: {
-				dpi: 300,
-				scale: 4,
-				letterRendering: true,
-				useCORS: true
-			},
-			jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-		};
-		html2pdf().set(opt).from(content).save();
-	}
-	
+	function print() {
+        html2pdf()
+          .set({
+            image: { type: "jpeg", quality: 1 },
+            pagebreak: {mode: 'css',},
+            html2canvas: { scale: 4 },
+            jsPDF: { unit: "mm", format: "A4", orientation: "portrait" },
+          })
+          .from(document.getElementsByTagName("pdf")[0])
+          .save("faktura.pdf");
+      }
 </script>
 
 
 <svelte:head>
-	<script src=
-		"https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js">
-    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
 </svelte:head>
 
-<div class="overflow-auto p-8 flex flex-col justify-center items-center">
-	
-	<div class="flex gap-12">
-		<button on:click={() => goto("/")}>
-			Zpět
-		</button>
+<button class="print" on:click={() => print()}>Stáhnout PDF</button>
 
 
-		<button on:click={() => printing()}>
-			Stáhnout PDF
-		</button>
-	</div>
-	
-	<div id="content" class=" w-[21cm] h-[29.68cm] p-8 bg-white border text-black">
-		<div class="flex justify-end gap-32">
-			<p class="text-xl font-bold">
-				FAKTURA - daňový doklad
-			</p>
 
-			<p class="text-xl font-bold">
-				{data.invoiceNumber}
-			</p>
-		</div>
+<pdf>
+	<header>header</header>
 
-		<p class="text-sm text-center">Objednávka č. {data.orderNumber}</p>
-		
+	<content>
+		<ul class="list">
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+			<li>Item</li>
+		</ul>
+	</content>
 
-    </div>
+	<footer>footer</footer>
+</pdf>
 
-</div>
-	
+
+ <style>
+      @page {
+        size: A4;
+        margin: 0;
+      }
+      .print {
+        position: absolute;
+        top: 50px;
+        right: 50px;
+	  }
+      pdf > header {
+        position: fixed;
+        width: 100%;
+        top: 0;
+        height: 20px;
+        background: red;
+      }
+      pdf > footer {
+        position: fixed;
+        width: 100%;
+        bottom: 0;
+        height: 40px;
+        background: blue;
+      }
+      pdf > content {
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
+</style>
