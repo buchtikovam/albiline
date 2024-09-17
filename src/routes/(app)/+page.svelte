a2ac7d315be2e663869aa6fce611e303b2c4c684
Monika Buchtíková <buchtikovam@email.cz>
2024-09-17 15:54:39 +0200

    .

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col bg-white flex-1 items-center justify-center h-dvh rounded-lg border border-albi-200 mr-4">
+<div class="flex flex-col bg-white flex-1 items-center justify-center h-dvh rounded-lg border border-albi-200 ">
 	<h1 class="mb-2 text-xl font-bold text-albi-500 dark:text-albi-500">
 	</h1>
 </div>
