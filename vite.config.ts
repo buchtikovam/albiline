ed04efcb5abad2f55dedcfdbd6cc0f49f8d49095
Monika Buchtíková <buchtikovam@email.cz>
2025-06-25 14:08:30 +0200

    npm updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import {defineConfig} from 'vite';
 import {SvelteKitPWA} from "@vite-pwa/sveltekit";
 const env = process.env;
 import devtoolsJson from 'vite-plugin-devtools-json';
+// import basicSsl from "@vitejs/plugin-basic-ssl"
 
 
 export default defineConfig({
 	plugins: [
+		// basicSsl(),
 		sveltekit(),
 		devtoolsJson(),
 		paraglideVitePlugin({
